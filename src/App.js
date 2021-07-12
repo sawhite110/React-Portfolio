710dc0161bff2c8ab0757fbf59971b0b45a9ab47
SAW2Codes <stevenson.whiteii@gmail.com>
2021-07-12 00:44:35 -0400

    Updated NavTabs.js and added images to public folder.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 // import PortfolioContainer from "./components/PortfolioContainer";
 // import About from "./components/pages/About";
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavTabs />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/portfilio">
+            <Portfolio />           
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>     
+      </Router>
     </div>
   );
 }
